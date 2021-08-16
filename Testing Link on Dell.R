309d25482bae9bb533f2964311dad66cc344a37c
Lucinbell <dylanfox1@gmail.com>
2021-08-16 14:53:28 +0900

    Just testing

--- Testing Link on Dell.R
+++ Testing Link on Dell.R
@@ -1,2 +1,6 @@
 print("this is just to test if the GitHub link is working")
 print("God I hope it does, otherwise I am gonna cry")
+
+Print("Now I am testing link on my company Laptop")
+
+Print("Hello!")
