b8eebd4485e17339ebcb11da42e5a384c4b3ae5e
Lucinbell <dylanfox1@gmail.com>
2021-08-18 16:18:39 -0400

    Push awawy!

--- Testing Link on Dell.R
+++ Testing Link on Dell.R
@@ -1,2 +1,7 @@
 print("this is just to test if the GitHub link is working")
 print("God I hope it does, otherwise I am gonna cry")
+
+filepath <- paste("specdata", "/", x,".csv", sep = "")
+
+## Got a new laptop. Everything seems to have been set up. Now to test
+## if Git actually works. Push away!
