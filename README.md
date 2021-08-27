d3adb8f1a96baa2d687377da5789e2b9b507cb59
Lucinbell <dylanfox1@gmail.com>
2021-08-26 22:11:48 -0400

    Week 4 assignment submission for 'Getting and Cleaning Data' course on Coursera.

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# DataScienceCoursera
-Proving ground for all data science assignments, lessons, practice moving forward
+## Getting and Cleaning Data > Week 4 Assignment
+## By Lucinbell
 
-Course in-progress: R Programming
+This repo is an assignment submission for week four of 'Getting and Cleaning
+Data' on Coursera.
+
+The repo contains three items:
+
+1. README.md
+  This simple README file contains summary description of each component of 
+  the assignment.
+  
+2. CodeBook.md
+  This is the note describing the original data
+  
+3. run_analysis.R
+  The R script that executes the data cleaning and transformation of the
+  assignment dataset: UHI HAR Dataset. The R script performs download, unzip,
+  data merging, and calculation, with the final output of a .txt file.
+  The script requires packages "dplyr" (for grouping and transforming data)
+  and "utils" (for unziping compressed file) to function.
+  
+Running "run_analysis.R":
+- To run the script, simply download the script to the working directory and
+source the file. The script will check if the "UCI HAR Dataset" (the unzipped
+file from the downloaded file) is present in the working directory. If not,
+it will download and decompress the file.
+
---
+++ run_analysis.R
@@ -0,0 +1,79 @@
+# The run_analysis.R 
+# By Lucinbell, 2021/08/26
+
+# 0. Check to see if the assignment dataset is in the working directory.
+#    If not, download and unzip the file
+
+  if("UCI HAR Dataset" %in% dir()){
+    message("UCI HAR Dataset detected in dir()")
+  } else {
+    message("UCI HAR Dataset not found. Proceed to download dataset")
+    url1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    download.file(url1, destfile = "./Dataset.zip")
+    unzip("./Dataset.zip")
+    remove(url1)
+  }
+  
+
+# 1. Merges the training and the test sets to create one data set.
+  giant_table <- rbind(
+    read.table("./UCI HAR Dataset/test/X_test.txt"),
+    read.table("./UCI HAR Dataset/train/X_train.txt")
+  )
+  
+  
+  subject_string <- rbind(
+    read.table("./UCI HAR Dataset/test/subject_test.txt"),
+    read.table("./UCI HAR Dataset/train/subject_train.txt")
+  )
+  
+  activity_string <- rbind(
+    read.table("./UCI HAR Dataset/test/y_test.txt"),
+    read.table("./UCI HAR Dataset/train/y_train.txt")
+  )
+  
+
+# 2. Extracts only the measurements on the mean and standard
+#    deviation for each measurement.
+  features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+  targetcol <- grep("mean\\()|std\\()", features)
+
+  slim_table <- giant_table[, targetcol]
+    #remove unnecessary object
+    remove(giant_table)
+  
+# 3. Uses descriptive activity names to name the activities in the data set
+  activity <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+  for(i in 1:6){
+    activity_string[[1]] <- gsub(i, activity[i], activity_string[[1]])
+  }
+
+# 4. Appropriately labels the data set with descriptive variable names
+  colnames(slim_table) <- features[targetcol]
+  colnames(activity_string) <- "activity"
+  colnames(subject_string) <- "subject"
+  
+  slim_table <- cbind(subject_string, activity_string, slim_table)
+  
+  #remove un-needed objects
+    remove(subject_string, activity_string,
+           activity, features, targetcol, i)
+
+# 5. From the data set in step 4, creates a second, independent tidy data
+#    set with the average of each variable for each activity and each subject
+  if("dplyr" %in% (.packages())){
+    message("dplyr already loaded")
+  }  else {
+    message("Loading dplyr")
+    library(dplyr)
+  }
+  
+  output <- slim_table %>% group_by(subject, activity) %>% 
+            summarize(across(.cols= everything(), mean))
+
+  
+
+  write.table(output, file = "./tidy_averages.txt")
+
+  
+  
